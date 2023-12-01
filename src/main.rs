8e4ae5ceee8b8c27c0185157ba20c349a3a72353
aQaTL <mmsoltys@outlook.com>
2023-12-01 13:28:08 +0100

    Day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::extract::Path;
 use axum::{http::StatusCode, routing::get, Router};
 use shuttle_axum::AxumService;
 
@@ -6,7 +7,8 @@ async fn shuttle_main() -> shuttle_axum::ShuttleAxum {
 	let router = Router::new()
 		.route("/", get(nothing))
 		.route("/hello", get(hello_world))
-		.route("/-1/error", get(error_page));
+		.route("/-1/error", get(error_page))
+		.route("/1/*nums", get(day_1));
 	Ok(AxumService(router))
 }
 
@@ -20,4 +22,12 @@ async fn hello_world() -> &'static str {
 
 async fn error_page() -> (StatusCode, &'static str) {
 	(StatusCode::INTERNAL_SERVER_ERROR, "Internal server error")
+}
+
+async fn day_1(Path(nums): Path<String>) -> String {
+	nums.split('/')
+		.map(|x| x.parse::<i64>().unwrap())
+		.fold(0, |acc, x| acc ^ x)
+		.pow(3)
+		.to_string()
 }
